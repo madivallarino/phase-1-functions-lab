e3ea958f51acabd0a8705cc42bddb5eb7a5a81d0
Madison Vallarino <madivallarino@gmail.com>
2021-08-10 22:01:59 -0500

    commit tree eb7658c63fca72d6efbaf6400c51b46cf76dcb6b

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const headquarters = 42;
+
+function distanceFromHqInBlocks(a){
+    return Math.abs(headquarters - a);
+}
+
+function distanceFromHqInFeet(someValue){
+    return distanceFromHqInBlocks(someValue) * 264
+}
+
+function distanceTravelledInFeet(a, b){
+    return Math.abs(a - b) * 264
+}
+
+function calculatesFarePrice(start, destination){
+   const tripFeet = distanceTravelledInFeet (start, destination);
+   if (tripFeet <= 400){
+       return 0;
+   } else if (tripFeet > 400 & tripFeet <= 2000){
+       return (tripFeet - 400) * 0.02;
+   }else if (tripFeet > 2000 && tripFeet <= 2500){
+       return 25;
+   } else {
+       return "cannot travel that far";
+   }
+
+}
